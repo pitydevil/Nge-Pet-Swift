b058f71e304e6d6df9ecb626bc3a990bb5105f01
Mikhael Adiputra <mikhael_adiputra@icloud.com>
2022-09-29 13:37:45 +0700

    Add(Global): Folder Structures

---
+++ Fluffy/Utils/Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Extension.swift
+//  Fluffy
+//
+//  Created by Mikhael Adiputra on 29/09/22.
+//
+
+import Foundation
+
---
+++ Fluffy/Utils/Helper Class.swift
@@ -0,0 +1,8 @@
+//
+//  Helper Class.swift
+//  Fluffy
+//
+//  Created by Mikhael Adiputra on 29/09/22.
+//
+
+import Foundation
