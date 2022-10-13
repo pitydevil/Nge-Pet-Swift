693e73ebea30ad74a5c9e4505bfc0e57c67af7ba
Jessicaggan <jessica.ganadhi@binus.ac.id>
2022-10-13 17:25:03 +0700

    feat(Explore): fix constraints

--- Fluffy/Components/ExploreCard.swift
+++ Fluffy/Components/ExploreCard.swift
@@ -7,19 +7,6 @@
 
 import UIKit
 
-struct PetSupported{
-    let petType:String
-    let size:String
-}
-
-struct PetHotelList{
-    let petHotelName:String
-    let petHotelDistance:String
-    let supportedPet:[PetSupported]
-    let lowestPrice:Int
-    let displayImage:String
-}
-
 class ExploreCard: UIView {
 
     //MARK: - Properties
@@ -39,6 +26,10 @@ class ExploreCard: UIView {
         super.init(frame: .zero)
         setupLayout()
         self.backgroundColor = .white
+        self.backgroundColor = UIColor(named: "white")
+        self.layer.cornerRadius = 12
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.heightAnchor.constraint(equalToConstant: 216).isActive = true
     }
     
     required init?(coder: NSCoder) {
@@ -59,19 +50,25 @@ class ExploreCard: UIView {
     private lazy var priceLabel:ReuseableLabel = ReuseableLabel(labelText: "Rp 30.000", labelType: .titleH2, labelColor: .primaryMain)
 
     private lazy var supportedPetView:UICollectionView = {
-        let collection = UICollectionView()
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
         collection.showsHorizontalScrollIndicator = false
         collection.dataSource = self
         collection.delegate = self
-        
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.register(SupportedPetCollectionViewCell.self, forCellWithReuseIdentifier: SupportedPetCollectionViewCell.cellId)
+//        collection.
         return collection
     }()
 }
 
+//MARK: - Setups
+
 extension ExploreCard{
     fileprivate func setup() {
         petHotelNameLabel.text = petHotelName
+        petHotelNameLabel.numberOfLines = 1
         distanceLabel.text = distance
+        distanceLabel.numberOfLines = 1
         exploreImage.image = UIImage(named: displayImage)
         
         let formatter = NumberFormatter()
@@ -98,11 +95,12 @@ extension ExploreCard{
         petHotelNameLabel.leftAnchor.constraint(equalTo: exploreImage.rightAnchor, constant: 20).isActive = true
         petHotelNameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
         petHotelNameLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -20).isActive = true
+        petHotelNameLabel.heightAnchor.constraint(equalToConstant: 23).isActive = true
     }
     
     fileprivate func distanceConstraints() {
         distanceLabel.leftAnchor.constraint(equalTo: exploreImage.rightAnchor, constant: 20).isActive = true
-        distanceLabel.topAnchor.constraint(equalTo: petHotelNameLabel.bottomAnchor, constant: 8).isActive = true
+        distanceLabel.topAnchor.constraint(equalTo: petHotelNameLabel.bottomAnchor, constant: 4).isActive = true
         distanceLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -20).isActive = true
     }
     
@@ -133,21 +131,32 @@ extension ExploreCard{
         self.addSubview(redRectangle)
         self.addSubview(priceLabel)
         self.addSubview(BeforePriceLabel)
-        
+        self.addSubview(supportedPetView)
         imageConstraints()
         petHotelNameConstraints()
         distanceConstraints()
         priceConstraints()
         
+        supportedPetView.bottomAnchor.constraint(equalTo: exploreImage.bottomAnchor).isActive = true
+        supportedPetView.leftAnchor.constraint(equalTo: exploreImage.rightAnchor, constant: 20).isActive = true
+        supportedPetView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -20).isActive = true
+        supportedPetView.heightAnchor.constraint(equalToConstant: 68).isActive = true
+        supportedPetView.backgroundColor = .clear
+        
     }
 }
-
+//MARK: -UICollectionViewDelegate, UICollectionViewDataSource
 extension ExploreCard: UICollectionViewDelegate, UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        //ini ubah
+        return supportedPet.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SupportedPetCollectionViewCell.cellId, for: indexPath) as? SupportedPetCollectionViewCell else { return UICollectionViewCell() }
+        
+        //Ini ubah
+        cell.configure(petTypeString: supportedPet[indexPath.row].petType, petSizeString: supportedPet[indexPath.row].size)
+        return cell
     }
 }
--- Fluffy/Components/SupportedPetCollectionViewCell.swift
+++ Fluffy/Components/SupportedPetCollectionViewCell.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 class SupportedPetCollectionViewCell: UICollectionViewCell {
+    
     // MARK: - SubViews
     private lazy var imageView = UIImageView()
     private lazy var petType:ReuseableLabel = ReuseableLabel(labelText: "Anjing", labelType: .titleH3, labelColor: .grey1)
     private lazy var petSize:ReuseableLabel = ReuseableLabel(labelText: "S,M", labelType: .bodyP2, labelColor: .grey1)
     
     // MARK: - Properties
-    static let cellId = "CarouselCollectionViewCell"
+    static let cellId = "SupportedPetCollectionViewCell"
     
     // MARK: - Initializer
     override init(frame: CGRect) {
@@ -31,16 +32,7 @@ class SupportedPetCollectionViewCell: UICollectionViewCell {
 // MARK: - Setups
 
 private extension SupportedPetCollectionViewCell {
-    func setupUI() {
-        backgroundColor = .clear
-        
-        addSubview(imageView)
-        addSubview(petType)
-        addSubview(petSize)
-        
-        self.heightAnchor.constraint(equalToConstant: 72).isActive = true
-        self.widthAnchor.constraint(equalToConstant: 68).isActive = true
-        
+    func imageViewConstraints() {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
         imageView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
@@ -49,17 +41,47 @@ private extension SupportedPetCollectionViewCell {
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 12
+    }
+    
+    func labelConstraints() {
+        petType.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        petType.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4).isActive = true
+        petType.widthAnchor.constraint(equalToConstant: 72).isActive = true
+        petType.numberOfLines = 1
         
+        petSize.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        petSize.topAnchor.constraint(equalTo: petType.bottomAnchor).isActive = true
+        petSize.widthAnchor.constraint(equalToConstant: 72).isActive = true
+        petSize.numberOfLines = 1
+    }
+    
+    func setupUI() {
+        backgroundColor = .clear
         
+        addSubview(imageView)
+        addSubview(petType)
+        addSubview(petSize)
+        
+        self.heightAnchor.constraint(equalToConstant: 68).isActive = true
+        self.widthAnchor.constraint(equalToConstant: 72).isActive = true
+        
+        imageViewConstraints()
+        labelConstraints()
     }
 }
 
 // MARK: - Public
 
 extension SupportedPetCollectionViewCell {
-    public func configure(image: UIImage?) {
-        imageView.image = image
+    public func configure(petTypeString:String, petSizeString:String) {
+        petType.text = petTypeString
+        petSize.text = petSizeString
+        if petTypeString == "Anjing"{
+            imageView.image = UIImage(named: "dog-icon")
+        }
+        else if petTypeString == "Kucing"{
+            imageView.image = UIImage(named: "cat-icon")
+        }
+        
     }
 }
-
-
