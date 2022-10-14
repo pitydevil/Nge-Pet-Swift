485b99fed9d141fed53d2a40fd738b6e3ad3caa0
Zacky Ilahi Azmi <zackyilahi.azmi@gmail.com>
2022-10-14 22:49:39 +0700

    feat(ReusableTabBar): add line feat(views): add modalmonitoringcontroller, add modalmonitoringtableviewcell

---
+++ Fluffy/Views/ModalMonitoringTableViewCell.swift
@@ -0,0 +1,77 @@
+//
+//  ModalMonitoringTableViewCell.swift
+//  Fluffy
+//
+//  Created by Zacky Ilahi Azmi on 13/10/22.
+//
+
+import UIKit
+
+class ModalMonitoringTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    static let cellId = "ModalMonitoringTableViewCell"
+    
+    var petName = ReuseableLabel(labelText: "Bom Bom", labelType: .bodyP2, labelColor: .grey1)
+    
+    let petIcon = UIImageView()
+    
+    let checkedImage = UIImageView()
+    
+    var isChecked: Bool = false
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        //MARK: - Setup Cell
+        self.backgroundColor = UIColor(named: "white")
+        self.selectionStyle = .none
+        
+        contentView.layer.cornerRadius = 8
+        
+        //MARK: - Setup Autoresizingmask false
+        petName.translatesAutoresizingMaskIntoConstraints = false
+        petIcon.translatesAutoresizingMaskIntoConstraints = false
+        checkedImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        //MARK: - Add Subview
+        contentView.addSubview(petName)
+        
+        contentView.addSubview(petIcon)
+        petIcon.image = UIImage(systemName: "pawprint.fill")
+        petIcon.tintColor = UIColor(named: "primaryMain")
+        
+        contentView.addSubview(checkedImage)
+        checkedImage.image = UIImage()
+        checkedImage.tintColor = UIColor(named: "primaryMain")
+        
+        NSLayoutConstraint.activate([
+            petIcon.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12),
+            petIcon.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            
+            petName.leadingAnchor.constraint(equalTo: petIcon.trailingAnchor, constant: 8),
+            petName.centerYAnchor.constraint(equalTo: petIcon.centerYAnchor),
+            
+            checkedImage.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -12),
+            checkedImage.centerYAnchor.constraint(equalTo: petIcon.centerYAnchor),
+        ])
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 12, right: 0))
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Fluffy/Views/ModalMonitoringViewController.swift
@@ -0,0 +1,139 @@
+//
+//  ModalMonitoringPetViewController.swift
+//  Fluffy
+//
+//  Created by Zacky Ilahi Azmi on 12/10/22.
+//
+
+import UIKit
+
+class ModalMonitoringPetViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    private lazy var indicator: UIImageView = {
+        let indicator = UIImageView()
+        let config = UIImage.SymbolConfiguration(pointSize: 40, weight: .regular)
+        indicator.image = UIImage(systemName: "minus", withConfiguration: config)
+        indicator.tintColor = UIColor(named: "grey2")
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        return indicator
+    }()
+    
+    private lazy var headline: ReuseableLabel = {
+        let headline = ReuseableLabel(labelText: "Pilih Hewan", labelType: .titleH1, labelColor: .black)
+        return headline
+    }()
+    
+    private lazy var modalTableView: UITableView = {
+        let modalTableView = UITableView()
+        modalTableView.delegate = self
+        modalTableView.dataSource = self
+        modalTableView.backgroundColor = UIColor(named: "white")
+        modalTableView.register(ModalMonitoringTableViewCell.self, forCellReuseIdentifier: ModalMonitoringTableViewCell.cellId)
+        modalTableView.translatesAutoresizingMaskIntoConstraints = false
+        modalTableView.separatorStyle = .none
+        return modalTableView
+    }()
+    
+    private lazy var customBar: ReusableTabBar = {
+        let customBar = ReusableTabBar(btnText: "Pilih", showText: .show, isChecked: false)
+        return customBar
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(named: "white")
+        
+        view.addSubview(indicator)
+        NSLayoutConstraint.activate([
+            indicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+        
+        view.addSubview(headline)
+        NSLayoutConstraint.activate([
+            headline.topAnchor.constraint(equalTo: view.topAnchor, constant: 48),
+            headline.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+        ])
+        
+        view.addSubview(modalTableView)
+        NSLayoutConstraint.activate([
+            modalTableView.topAnchor.constraint(equalTo: headline.bottomAnchor, constant: 0),
+            modalTableView.widthAnchor.constraint(equalToConstant: 342),
+            modalTableView.heightAnchor.constraint(equalToConstant: view.bounds.height),
+            modalTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+        ])
+        
+        view.addSubview(customBar)
+        NSLayoutConstraint.activate([
+            customBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            
+            customBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            customBar.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        customBar.barBtn.addTarget(self, action: #selector(petSelected), for: .touchUpInside)
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        var label = ReuseableLabel(labelText: "Sedang Dititipkan", labelType: .titleH2, labelColor: .black)
+        if section == 0 {
+            label = ReuseableLabel(labelText: "Sedang Dititipkan", labelType: .titleH2, labelColor: .black)
+        } else {
+            label = ReuseableLabel(labelText: "Belum Dititipkan", labelType: .titleH2, labelColor: .black)
+        }
+        headerView.addSubview(label)
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 36
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ModalMonitoringTableViewCell.cellId, for: indexPath)
+        if indexPath.section == 0 {
+            cell.contentView.backgroundColor = UIColor(named: "grey3")
+        } else {
+            cell.contentView.backgroundColor = UIColor(named: "grey1")?.withAlphaComponent(0.5)
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = modalTableView.cellForRow(at: indexPath) as! ModalMonitoringTableViewCell
+        if indexPath.section == 0 {
+            cell.isUserInteractionEnabled = true
+            if cell.isChecked == false {
+                cell.isChecked = true
+                cell.checkedImage.image = UIImage(systemName: "checkmark")
+            } else {
+                cell.isChecked = false
+                cell.checkedImage.image = UIImage()
+            }
+        } else {
+            cell.isUserInteractionEnabled = false
+            
+        }
+    }
+    
+    @objc func petSelected() {
+        dismiss(animated: true)
+    }
+    
+
+}
