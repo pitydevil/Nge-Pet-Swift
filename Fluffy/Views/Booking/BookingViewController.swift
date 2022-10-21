bca28d9489331498bb5a082441052c7530e30ac1
jessicaggan <80453796+jessicaggan@users.noreply.github.com>
2022-10-21 17:35:50 +0700

    feat(explore):  adjust constraint (#13)
    Co-authored-by: pitydevil <45563787+pitydevil@users.noreply.github.com>

--- Fluffy/Views/Booking/BookingViewController.swift
+++ Fluffy/Views/Booking/BookingViewController.swift
@@ -11,7 +11,7 @@ class BookingViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .yellow
         // Do any additional setup after loading the view.
     }
     
