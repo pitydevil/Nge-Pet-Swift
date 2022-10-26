4681b303c6dcb202de62ab5c4a9a57a6ae5f21c7
Jessicaggan <jessica.ganadhi@binus.ac.id>
2022-10-26 22:28:51 +0700

    add(Booking): Booking confirmation page

---
+++ Fluffy/Views/Booking/BookingConfirmationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookingConfirmationViewController.swift
+//  Fluffy
+//
+//  Created by Jessica Geofanie on 26/10/22.
+//
+
+import UIKit
+
+class BookingConfirmationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
