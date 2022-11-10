7b177595be49cead637960ca8b56ec6a7b193cf5
pitydevil <45563787+pitydevil@users.noreply.github.com>
2022-11-10 14:59:00 +0700

    Ngep 83 implement logic list pet hotel explore controller (#38)
    * feat(explore): add pet model on explore table view cell
    
    * fix(detail pesanan): modify detail pesanan endpoint structure

--- Fluffy/Views/Booking/Domain/CustomSOP.swift
+++ Fluffy/Views/Booking/Domain/CustomSOP.swift
@@ -9,14 +9,14 @@ import Foundation
 
 struct CustomSOP: Codable {
     let customSopID: Int
-    let customSopName, orderDetailID, monitoringID: String
+    let customSopName, orderDetailID : String
 }
 extension CustomSOP {
     enum CodingKeys: String, CodingKey {
         case customSopID = "custom_sop_id"
         case customSopName = "custom_sop_name"
         case orderDetailID = "order_detail_id"
-        case monitoringID = "monitoring_id"
+
     }
     
     init(from decoder: Decoder) throws {
@@ -24,6 +24,5 @@ extension CustomSOP {
         customSopID = try container.decode(Int.self, forKey: .customSopID)
         customSopName = try container.decode(String.self, forKey: .customSopName)
         orderDetailID = try container.decode(String.self, forKey: .orderDetailID)
-        monitoringID  = try container.decode(String.self, forKey: .monitoringID)
     }
 }
